d3f4c83debddca699f7c6d35ae947f6c171a5d0a
Chetan Bedi <chetanbedi141@gmail.com>
2025-01-25 19:06:37 +0530

    deploy test

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -152,7 +152,7 @@ const LoginPage = () => {
                   lineHeight: 1.385,
                 }}
               >
-                Welcome to Marc Admin! 👋🏻
+                Welcome to Marc Adminsss! 👋🏻
               </Typography>
               <Typography sx={{ color: "text.secondary" }}>
                 Please sign-in to your account and start ..
